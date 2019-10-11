07eca900179419aa14af66bdb4f57f220f1e0553
JohnCow <s93294@gmail.com>
2019-10-11 20:04:54 +0800

    Final version

--- Tetris.h
+++ Tetris.h
@@ -1,117 +1,184 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+class TetrisChain;
+class Node{
+    public:
+    friend class TetrisChain;
+    Node(int c)
+    :arr{new int [c]}, link{0}{
+        for (int i = 0; i < c; i++)
+            arr[i] = 0;
+    }
+    private:
+    int *arr;
+    Node* link;
+};
 class TetrisChain{
     public:
     TetrisChain(int r, int c)
-    : rows{r}, cols{c}, last{new rowNode(cols + 1)}, currentTop{0}{
-        for (int i = 0; i <= cols; i++)
-            last->rowArr[i] = 1;
-        last->down = last;
-        last->up = last;
+    :rows{r}, cols(c), first{0}, last{0}, chainLengh{0}{}
+    TetrisChain createChain(int, int*);
+    void addChain(TetrisChain);
+    void concatenate(TetrisChain);
+    bool isFull(Node*);
+    void deleteRow(Node* a, Node* b = 0);
+    void printBoard(ofstream&, Node*);
+    bool isEnd();
+    void checkBoard();
+    Node* getfirst();
+    int lengh(){
+        return chainLengh;
     }
-    bool isFull(rowNode*);
-    void deleteNode(rowNode*);
-    void insetFront();
-    void printBoard();
-    void insertT1(int);
-    
     private:
-    class rowNode{
-        public:
-        rowNode(int c)
-        :rowArr{new int [c + 1]}{
-            for (int i = 0; i < c + 1; i++)
-                rowArr[i] = 0;
-        }
-        int *rowArr;
-        rowNode *down, *up;
-    };
-    
-    rowNode *last;
-    int rows, cols;
-    int currentTop;
+    Node *first, *last;
+    int rows, cols, chainLengh;
 };
-
-bool TetrisChain::isFull(rowNode* a){
-    for (int i = 1; i < cols; i++){
-        if (i = 0)
+TetrisChain TetrisChain::createChain(int lengh, int* a){  
+    TetrisChain e(rows, cols);
+    for (int i = 1; i <= lengh; i++){
+        if (e.first){
+            e.last->link = new Node(cols);
+            e.last = e.last->link;
+        }else {
+            e.first = e.last = new Node(cols);
+        }
+    }
+    for (int i = 0; i < 4; i++){
+        Node* ptr = e.first;
+        for (int j = 0; j < a[i] / cols; j++, ptr = ptr->link);
+        ptr->arr[a[i] % cols] = 1;
+    }
+    e.chainLengh = lengh;
+    return e;
+}
+void TetrisChain::addChain(TetrisChain a){
+    if (first){
+        bool illegal = false;
+        for (int i = chainLengh; i >= 1; i--){
+            Node *ptr = first, *aPtr = a.first;
+            for (int j = 1; j < i; j++, ptr = ptr->link);
+            Node *iteratePtr = ptr;
+            for (int j = 1; j <= a.chainLengh && iteratePtr != 0; j++, aPtr = aPtr->link, iteratePtr = iteratePtr->link){
+                for (int k = 0; k < cols; k++){
+                    if (aPtr->arr[k] == 1 && iteratePtr->arr[k] == 1){
+                        illegal = true;
+                        break;
+                    }
+                }
+                if (illegal)
+                    break;    
+            }
+            if (illegal){
+                Node *temp;
+                aPtr = a.first;
+                for (; ptr->link != 0 && aPtr != 0; aPtr = temp, ptr = ptr->link){
+                    for (int i = 0; i < cols; i++){
+                        ptr->link->arr[i] += aPtr->arr[i];
+                    }
+                    temp = aPtr->link;
+                    a.deleteRow(aPtr);
+                }
+                if (aPtr != 0){
+                    concatenate(a);
+                }
+                break;
+            }
+        }
+        if (!illegal){
+            Node *aPtr = a.first, *temp;
+            for (Node *ptr = first; ptr != 0 && aPtr != 0; aPtr = temp, ptr = ptr->link){
+                for (int i = 0; i < cols; i++){
+                    ptr->arr[i] += aPtr->arr[i];
+                }
+                temp = aPtr->link;
+                a.deleteRow(a.first);
+            }
+            if (aPtr != 0){
+                concatenate(a);
+            }
+        }
+    }else {
+        concatenate(a);
+    }
+}
+void TetrisChain::concatenate(TetrisChain a){
+    if (first){
+        last->link = a.first;
+        last = a.last;
+        chainLengh += a.chainLengh;
+    }else{
+        first = a.first;
+        last = a.last;
+        chainLengh = a.chainLengh;
+    }
+    a.first = a.last = 0;
+}
+bool TetrisChain::isFull(Node* a){
+    for (int i = 0; i < cols; i++){
+        if (a->arr[i] == 0)
             return false;
     }
     return true;
 }
-
-void TetrisChain::deleteNode(rowNode* a){
-    a->down->up = a->up;
-    a->up->down = a->down;
-    delete a;
-}
-
-void TetrisChain::insetFront(){
-    rowNode *newNode = new rowNode(cols);
-    newNode->down = last->down;
-    newNode->up = last;
-    last->down->up = newNode;
-    last->down = newNode;
-    currentTop++;
-}
-
-void TetrisChain::printBoard(){
-    for (int i = 1; i <= rows - currentTop; i++){
-        for (int j = 1; j <= cols; j++){
-            cout << 0;
+void TetrisChain::deleteRow(Node* a, Node* b){
+    if (a == first){
+        if (a == last)
+            first = last = 0;
+        else {
+            first = first->link;
         }
-        cout << endl;
     }
-    for (rowNode *ptr = last->down; ptr != last; ptr = ptr->down){
-        for (int i = 1; i <= cols; i++){
-            cout << ptr->rowArr[i];
+    else{
+        if (a == last){
+            last = b;
+            b->link = 0;
+        }else{
+            b->link = a->link;
         }
-        cout << endl;
-    }
+    } 
+    chainLengh--;
+    delete a;    
 }
-
-void TetrisChain::insertT1(int col){
-    
-    rowNode *ptr = last->down;
-    while (!ptr->rowArr[col] && !ptr->rowArr[col + 1] && !ptr->rowArr[col + 2])
-        ptr = ptr->down;
-    
-    int block[2][3] = {col, col + 1, col + 2, 0, col + 1, 0};
-    if (ptr->rowArr[col + 1]){
-        int i = 1;
-        for (;i >= 0 && ptr->up != last; i--, ptr = ptr->up){
-            for (int j = 0; j < 3; j++){
-                ptr->up->rowArr[block[i][j]] = 1;
-            }
+void TetrisChain::printBoard(ofstream &ofile, Node* ptr){
+    static int call = 1;
+    if (ptr == 0 || call > rows){
+        for (int i = 1; i <= rows - chainLengh; i++){
+            for (int j = 0; j < cols; j++)
+                ofile << 0;
+            ofile << endl;
         }
-        for (; i >= 0; i--){
-            insetFront();
-            for (int j = 0; j < 3; j++){
-                last->down->rowArr[block[i][j]] = 1;
-            }
-        } 
     }else {
-        int i = 1;
-        for (int j = 0; j < 3; j++){
-            ptr->rowArr[block[i][j]] = 1;
-        }
-        i--;
-        for (;i >= 0 && ptr->up != last; i--, ptr = ptr->up){
-            for (int j = 0; j < 3; j++){
-                ptr->up->rowArr[block[i][j]] = 1;
-            }
-        }
-        for (; i >= 0; i--){
-            insetFront();
-            for (int j = 0; j < 3; j++){
-                last->down->rowArr[block[i][j]] = 1;
-            }
+        call++;
+        printBoard(ofile, ptr->link);
+        for (int i = 0; i < cols; i++)
+            ofile << ptr->arr[i];
+        ofile << endl;
+    }
+}
+bool TetrisChain::isEnd(){
+    return (chainLengh > rows);
+}
+void TetrisChain::checkBoard(){
+    
+    for (Node *ptr = first; ptr != 0;){
+        if (isFull(ptr)){
+            if (ptr == first){
+                deleteRow(ptr);
+                ptr = first;
+            }else{
+                Node* m = first;
+                for (; m->link != ptr; m = m->link);
+                deleteRow(ptr, m);
+                ptr = m->link;
+            }    
+        }else {
+            ptr = ptr->link;
         }
     }
 }
-
-
-
-
+inline Node* TetrisChain::getfirst(){
+    return first;
+}
--- main.cpp
+++ main.cpp
@@ -1,135 +1,143 @@
 #include <iostream>
-#include "Tetris_v.h"
+#include "Tetris.h"
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    fstream file;
+    ofstream ofile;
+    file.open("Tetris.data", ios::in);
+    ofile.open("Tetris.output", ios::out);
     int rows, cols, col;
     string move;
-    cin >> rows >> cols;
+    file >> rows >> cols;
     TetrisChain game(rows, cols);
-    cin >> move;
-    while (move != "END"){
+    file >> move;
+    while (move != "End"){
         if (move == "T1"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+1, cols+col, cols+col+1, cols+col+2};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "T2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+1, cols+col, cols+col+1, 2*cols+col+1};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "T3"){
-            cin >> col;
+            file >> col;
             col--;
-            int T[] = {col, col+1, cols+2, cols+col+1};
+            int T[] = {col, col+1, col+2, cols+col+1};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
-        }else if (move == "T3"){
-            cin >> col;
+        }else if (move == "T4"){
+            file >> col;
             col--;
             int T[] = {col, cols+col, cols+col+1, 2*cols+col};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "L1"){
-            cin >> col;
+            file >> col;
             col--;
-            int T[] = {col, cols+1, cols+col, 2*cols+col};
+            int T[] = {col, col+1, cols+col, 2*cols+col};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "L2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, cols+col, cols+col+1, cols+col+2};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "L3"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+1, cols+col+1, 2*cols+col, 2*cols+col+1};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "L4"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, col+1, col+2, cols+col+2};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "J1"){
-            cin >> col;
+            file >> col;
             col--;
-            int T[] = {col+1, cols+col, cols+col+1, 2*cols+col+1};
+            int T[] = {col, col + 1, cols+col+1, 2*cols+col+1};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "J2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, col+1, col+2, cols+col};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "J3"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, cols+col, 2*cols+col, 2*cols+col+1};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "J4"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+2, cols+col, cols+col+1, cols+col+2};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "S1"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, col+1, cols+col+1, cols+col+2};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "S2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+1, cols+col, cols+col+1, 2*cols+col};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "Z1"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col+1, col+2, cols+col, cols+col+1};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }else if (move == "Z2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, cols+col, cols+col+1, 2*cols+col+1};
             TetrisChain block = game.createChain(3, T);
             game.addChain(block);
         }else if (move == "I1"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, cols+col, 2*cols+col, 3*cols+col};
             TetrisChain block = game.createChain(4, T);
             game.addChain(block);
         }else if (move == "I2"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, col+1, col+2, col+3};
             TetrisChain block = game.createChain(1, T);
             game.addChain(block);
         }else if (move == "O"){
-            cin >> col;
+            file >> col;
             col--;
             int T[] = {col, col+1, cols+col, cols+col+1};
             TetrisChain block = game.createChain(2, T);
             game.addChain(block);
         }
-        cin >> move;
+        game.checkBoard();
+        if (game.isEnd())
+            break;
+        file >> move;
     }
     Node* start = game.getfirst();
-    game.printBoard(start);
+    game.printBoard(ofile , start);
     return 0;
 }
